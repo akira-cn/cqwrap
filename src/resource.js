5118809b0f6ff35d4d45d167df73e41781b630d0
akira_cn <akira.cn@gmail.com>
2014-02-17 23:12:45 +0800

    update

--- src/resource.js
+++ src/resource.js
@@ -1,10 +1,12 @@
 var g_resources = [
     //image
-    {src: 'res/img/HelloWorld.png'},
-    {src: 'res/img/CloseNormal.png'},
     {src: 'res/img/background.jpg'},
     {src: 'res/img/director.png'},
     {src: 'res/img/director2.png'},
+    {src: 'res/img/code_bird_0.png'},
+    {src: 'res/img/code_bird_1.png'},
+    {src: 'res/img/code_bird_2.png'},
+    {src: 'res/img/flappy_packer.png'},
     //plist
     {src: 'res/img/flappy_packer.plist'},
     {src: "res/img/p_rune_fire.plist"},
